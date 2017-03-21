98e0066acc7964f14233df3fbe88cab0378b7a55
TooSchoolForCool <zeyuz@outlook.com>
2017-03-21 18:23:56 +0800

    Add make manu function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,37 @@
 
 #include "dfa.h"
 
+using namespace std;
+
+string makeManu()
+{
+	string ret = "";
+	string stars(30, '*');
+
+
+	ret += stars + '\n';
+	ret += "\tManu\n";
+	ret += stars + '\n';
+
+	ret += "1. print all accepted string.\n";
+	ret += "2. check string.\n";
+	ret += "0. quit.\n";
+	ret += "please enter your choice: ";
+
+	return ret;
+}
+
 int main()
 {
-	cout << "Hello world" << endl;
+	DFA dfa;
+
+	dfa.load("test_dfa_1.dfa");
+
+	dfa.showDFA();
+
+	cout << makeManu() << endl;
+
+	dfa.save("dfa_out_1.dfa");
+
+	return 0;
 }
